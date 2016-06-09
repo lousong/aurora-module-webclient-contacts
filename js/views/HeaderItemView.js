689c643557117020bfc77fa735667ae2944b0b36
vasil <vasil.sokolov@gmail.com>
2016-06-09 17:59:48 +0300

    init

---
+++ js/views/HeaderItemView.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var
+	TextUtils = require('modules/CoreClient/js/utils/Text.js'),
+	
+	CHeaderItemView = require('modules/CoreClient/js/views/CHeaderItemView.js')
+;
+
+module.exports = new CHeaderItemView(TextUtils.i18n('%MODULENAME%/ACTION_SHOW_CONTACTS'));
