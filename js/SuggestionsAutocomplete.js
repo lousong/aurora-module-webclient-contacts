16f184ffd7c4045c7d67e33a73454e9a7bfe1ba9
sash04ek <a.ovcharov@gmail.com>
2020-10-03 09:32:43 +0300

    added GetContactSuggestions method

--- js/SuggestionsAutocomplete.js
+++ js/SuggestionsAutocomplete.js
@@ -30,7 +30,7 @@ function Callback(oRequest, fResponse, sExceptEmail, sStorage, bWithGroups)
 		}
 	;
 
-	Ajax.send('GetContacts', oParameters, function (oResponse) {
+	Ajax.send('GetContactSuggestions', oParameters, function (oResponse) {
 		var aList = [];
 		if (oResponse && oResponse.Result && oResponse.Result.List)
 		{
