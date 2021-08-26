5ac0a8106190383faf292b0f68e7e5453ef0bf87
nadine-shi <nadinshi@gmail.com>
2021-08-26 16:52:38 +0300

    Ability to work with several custom address books (delete)

--- js/views/AddressBooksSettingsFormView.js
+++ js/views/AddressBooksSettingsFormView.js
@@ -4,11 +4,14 @@ var
 	_ = require('underscore'),
 	ko = require('knockout'),
 	
-	Types = require('%PathToCoreWebclientModule%/js/utils/Types.js'),
+	TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
 	
 	Ajax = require('%PathToCoreWebclientModule%/js/Ajax.js'),
 	Api = require('%PathToCoreWebclientModule%/js/Api.js'),
+	ConfirmPopup = require('%PathToCoreWebclientModule%/js/popups/ConfirmPopup.js'),
+	Popups = require('%PathToCoreWebclientModule%/js/Popups.js'),
 	ModulesManager = require('%PathToCoreWebclientModule%/js/ModulesManager.js'),
+
 	CAbstractSettingsFormView = ModulesManager.run('SettingsWebclient', 'getAbstractSettingsFormViewClass')
 ;
 
@@ -29,10 +32,14 @@ CAddressBooksSettingsFormView.prototype.ViewTemplate = '%ModuleName%_AddressBook
 
 CAddressBooksSettingsFormView.prototype.onShow = function ()
 {
+	this.populate();
+};
+
+CAddressBooksSettingsFormView.prototype.populate = function ()
+{
 	this.loading(true);
 	Ajax.send('Contacts', 'GetAddressBooks', {}, function (oResponse) {
 		this.loading(false);
-		console.log('oResponse', oResponse);
 		if (_.isArray(oResponse && oResponse.Result)) {
 			this.addressBooks(oResponse.Result);
 		} else {
@@ -46,9 +53,24 @@ CAddressBooksSettingsFormView.prototype.addAddressBook = function ()
 	console.log('addAddressBook');
 };
 
-CAddressBooksSettingsFormView.prototype.deleteAddressBook = function (sId)
+CAddressBooksSettingsFormView.prototype.deleteAddressBook = function (sEntityId, sDisplayName)
 {
-	console.log('deleteAddressBook', sId);
+	var
+		sConfirm = TextUtils.i18n('%MODULENAME%/CONFIRM_DELETE_ADDRESSBOOK', { 'NAME': sDisplayName }),
+		fOnConfirm = _.bind(function (bOk) {
+			if (bOk)
+			{
+				Ajax.send('Contacts', 'DeleteAddressBook', {'EntityId': sEntityId}, function (oResponse) {
+					if (!oResponse || !oResponse.Result) {
+						Api.showErrorByCode(oResponse);
+					}
+					this.populate();
+				}, this);
+			}
+		}, this)
+	;
+
+	Popups.showPopup(ConfirmPopup, [sConfirm, fOnConfirm]);
 };
 
 module.exports = new CAddressBooksSettingsFormView();
